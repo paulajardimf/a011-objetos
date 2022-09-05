06c9073c69282afe974757c95bf8313825dd82ee
Paula <paulajardimf@gmail.com>
2022-09-05 13:19:38 -0300

    exercícios finalizados

---
+++ exercicio1/script.js
@@ -0,0 +1,19 @@
+// Crie um array vazio chamado `sacolao`, e siga os passos abaixo:
+const sacolao = [];
+
+// a) Crie três objetos que vão representar frutas do sacolão:
+sacolao.push({
+    nome: "banana",
+    preco: 9.00,
+    disponibilidade: true
+}, {
+    nome: "maçã",
+    preco: 5.00,
+    disponibilidade: true
+}, {
+    nome: "mamão",
+    preco: 8.54,
+    disponibilidade: false
+})
+
+console.log(sacolao);
